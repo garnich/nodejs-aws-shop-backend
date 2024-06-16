7ab44676d57afe711573848b420b7245221b4eb3
Andrei Ihnatsiuk <garnich@tut.by>
2024-06-16 09:15:24 +0300

    task-3: added API gateway

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NodejsFunction, NodejsFunctionProps } from "aws-cdk-lib/aws-lambda-nodejs";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,6 +25,27 @@ export class ProductServiceStack extends cdk.Stack {
       ...commonProps,
       entry: 'handlers/getProductById.ts',
       functionName: 'getProductById',
+    });
+
+    const apiGateway = new cdk.aws_apigatewayv2.HttpApi(this, 'GetProductsListApi', {
+      apiName: 'GetProductsListApi',
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowOrigins: ['*'],
+        allowMethods: [cdk.aws_apigatewayv2.CorsHttpMethod.GET],
+      }
+    });
+
+    apiGateway.addRoutes({
+      path: '/products',
+      methods: [cdk.aws_apigatewayv2.HttpMethod.GET],
+      integration: new HttpLambdaIntegration('GetProductsListIntegration', getProductsList),
+    });
+
+    apiGateway.addRoutes({
+      path: '/products/{productId}',
+      methods: [cdk.aws_apigatewayv2.HttpMethod.GET],
+      integration: new HttpLambdaIntegration('GetProductByIdIntegration', getProductById),
     });
   }
 }
