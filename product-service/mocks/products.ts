84d8177bea3ffe4058f1ec4abe1dbcf62ba0eb9f
Andrei Ihnatsiuk <garnich@tut.by>
2024-06-16 09:08:16 +0300

    task-3: fixed types naming

--- product-service/mocks/products.ts
+++ product-service/mocks/products.ts
@@ -1,6 +1,6 @@
-import  { Product } from '../types';
+import  { IProduct } from '../types';
 
-export const products: Product[] = [
+export const products: IProduct[] = [
     {
       description: "Short Product Description1",
       id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
