92f4ca3d946cf50fec7672a23b27b7c14fa8a431
Andrei Ihnatsiuk <garnich@tut.by>
2024-06-30 13:00:09 +0300

    task-5: updated import-service-stack file

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,91 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Function,
+  Runtime,
+  AssetCode,
 
-export class ImportServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+} from "aws-cdk-lib/aws-lambda";
+import {
+  LambdaIntegration,
+  RestApi,
+  Cors,
+} from "aws-cdk-lib/aws-apigateway";
+import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
+import { Bucket, HttpMethods, EventType, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
+
+export class ImportServiceStackGarnichApp extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+  
+      const bucket = new Bucket(this, "ProductImportBucket",
+        {
+      bucketName:  'garnich-import-service-bucket',
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        cors: [
+          {
+            maxAge: 60 * 60,
+            allowedOrigins: ['*'],
+            allowedMethods: [HttpMethods.GET, HttpMethods.PUT, HttpMethods.DELETE],
+            allowedHeaders: ['*'],
+          },
+        ],
+      }
+    );
 
-    // The code that defines your stack goes here
+      const importProductsLambda = new Function(this, 'importProductsFunction', {
+        runtime: Runtime.NODEJS_20_X,
+        code: new AssetCode('lambda'),
+        handler: 'importProductsFile.handler',
+        environment: {
+          BUCKET_NAME: bucket.bucketName,
+        },
+      });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+      const importParserLambda = new Function(this, 'ImportParserFunction', {
+        runtime: Runtime.NODEJS_20_X,
+        code: new AssetCode('lambda'),
+        handler: 'importFileParser.handler',
+        environment: {
+          BUCKET_NAME: bucket.bucketName,
+        },
+      });
+
+      const importProductsPolicy = new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ['s3:PutObject'],
+        resources: [bucket.bucketArn + '/*'],
+      });
+
+      importProductsLambda.addToRolePolicy(importProductsPolicy);
+  
+      const importParserPolicy = new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
+        resources: [bucket.bucketArn + '/*'],
+      });
+
+      importParserLambda.addToRolePolicy(importParserPolicy);
+
+      const api = new RestApi(this, "ImportService", {
+        restApiName: "ImportService",
+        cloudWatchRole: true,
+        defaultCorsPreflightOptions: {
+          allowOrigins: Cors.ALL_ORIGINS,
+          allowMethods: Cors.ALL_METHODS,
+        },
+      });
+
+
+    const importResource = api.root.addResource('import');
+
+    importResource.addMethod('GET', new LambdaIntegration(importProductsLambda));
+
+    bucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new LambdaDestination(importParserLambda),
+      { prefix: 'uploaded/' }
+    );
+    }
+};
