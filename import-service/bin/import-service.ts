98910b565b49c8de5ab64b14c740b613733abe0e
Andrei Ihnatsiuk <garnich@tut.by>
2024-06-30 13:00:19 +0300

    task-5: updated import-service file

--- import-service/bin/import-service.ts
+++ import-service/bin/import-service.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ImportServiceStack } from '../lib/import-service-stack';
+import { ImportServiceStackGarnichApp } from '../lib/import-service-stack';
 
 const app = new cdk.App();
-new ImportServiceStack(app, 'ImportServiceStack', {
+new ImportServiceStackGarnichApp(app, 'ImportServiceStackGarnichApp', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
